48b50366479be8800d9897218593317de7560ea7
Gustavo Casanova <gustavo.casanova@nicebots.com>
2020-09-25 01:04:27 -0300

    Fix comments

--- src/hw-mapping.h
+++ src/hw-mapping.h
@@ -14,38 +14,42 @@
 #if defined(__AVR_ATtiny25__) | \
     defined(__AVR_ATtiny45__) | \
     defined(__AVR_ATtiny85__)
-#define DDR_USI DDRB
-#define PORT_USI PORTB
-#define PIN_USI PINB
-#define PORT_USI_SDA PB0
-#define PORT_USI_SCL PB2
-#define PIN_USI_SDA PINB0
-#define PIN_USI_SCL PINB2
-#define TWI_START_COND_FLAG USISIF  // Status register flag: indicates an I2C START condition on the bus (can trigger an interrupt)
-#define USI_OVERFLOW_FLAG USIOIF    // Status register flag: indicates a complete bit reception/transmission (can trigger an interrupt)
-#define TWI_STOP_COND_FLAG USIPF    // Status register flag: indicates an I2C STOP condition on the bus
-#define TWI_COLLISION_FLAG USIDC    // Status register flag: indicates a data output collision on the bus
-#define TWI_START_COND_INT USISIE   // Control register bit: defines whether an I2C START condition triggers an interrupt
-#define USI_OVERFLOW_INT USIOIE     // Control register bit: defines whether a USI 4-bit counter overflow triggers an interrupt
+#define DDR_USI DDRB                      // I2C data direction register
+#define PORT_USI PORTB                    // I2C output register
+#define PIN_USI PINB                      // I2C input register
+#define PORT_USI_SDA PB0                  // I2C SDA output register
+#define PORT_USI_SCL PB2                  // I2C SCL output register
+#define PIN_USI_SDA PINB0                 // I2C SDA input register
+#define PIN_USI_SCL PINB2                 // I2C SDL input register
+#define TWI_START_COND_FLAG USISIF        // Status register flag: indicates an I2C START condition on the bus (can trigger an interrupt)
+#define USI_OVERFLOW_FLAG USIOIF          // Status register flag: indicates a complete bit reception/transmission (can trigger an interrupt)
+#define TWI_STOP_COND_FLAG USIPF          // Status register flag: indicates an I2C STOP condition on the bus
+#define TWI_COLLISION_FLAG USIDC          // Status register flag: indicates a data output collision on the bus
+#define TWI_START_COND_INT USISIE         // Control register bit: defines whether an I2C START condition triggers an interrupt
+#define USI_OVERFLOW_INT USIOIE           // Control register bit: defines whether a USI 4-bit counter overflow triggers an interrupt
+#define USI_START_VECTOR USI_START_vect   // Interrupt vector: USI start condition
+#define USI_OVERFLOW_VECTOR USI_OVF_vect  // Interrupt vector: USI 4-bit counter overflow
 #endif
 
 // ATtinyX4
 #if defined(__AVR_ATtiny24__) | \
     defined(__AVR_ATtiny44__) | \
     defined(__AVR_ATtiny84__)
-#define DDR_USI DDRA
-#define PORT_USI PORTA
-#define PIN_USI PINA
-#define PORT_USI_SDA PORTA6
-#define PORT_USI_SCL PORTA4
-#define PIN_USI_SDA PINA6
-#define PIN_USI_SCL PINA4
-#define TWI_START_COND_FLAG USISIF  // Status register flag: indicates an I2C START condition on the bus (can trigger an interrupt)
-#define USI_OVERFLOW_FLAG USIOIF    // Status register flag: indicates a complete bit reception/transmission (can trigger an interrupt)
-#define TWI_STOP_COND_FLAG USIPF    // Status register flag: indicates an I2C STOP condition on the bus
-#define TWI_COLLISION_FLAG USIDC    // Status register flag: indicates a data output collision on the bus
-#define TWI_START_COND_INT USISIE   // Control register bit: defines whether an I2C START condition triggers an interrupt
-#define USI_OVERFLOW_INT USIOIE     // Control register bit: defines whether a USI 4-bit counter overflow triggers an interrupt
+#define DDR_USI DDRA                      // I2C data direction register
+#define PORT_USI PORTA                    // I2C output register
+#define PIN_USI PINA                      // I2C input register
+#define PORT_USI_SDA PORTA6               // I2C SDA output register
+#define PORT_USI_SCL PORTA4               // I2C SCL output register
+#define PIN_USI_SDA PINA6                 // I2C SDA input register
+#define PIN_USI_SCL PINA4                 // I2C SDL input register
+#define TWI_START_COND_FLAG USISIF        // Status register flag: indicates an I2C START condition on the bus (can trigger an interrupt)
+#define USI_OVERFLOW_FLAG USIOIF          // Status register flag: indicates a complete bit reception/transmission (can trigger an interrupt)
+#define TWI_STOP_COND_FLAG USIPF          // Status register flag: indicates an I2C STOP condition on the bus
+#define TWI_COLLISION_FLAG USIDC          // Status register flag: indicates a data output collision on the bus
+#define TWI_START_COND_INT USISIE         // Control register bit: defines whether an I2C START condition triggers an interrupt
+#define USI_OVERFLOW_INT USIOIE           // Control register bit: defines whether a USI 4-bit counter overflow triggers an interrupt
+#define USI_START_VECTOR USI_STR_vect     // Interrupt vector: USI start condition
+#define USI_OVERFLOW_VECTOR USI_OVF_vect  // Interrupt vector: USI 4-bit counter overflow
 #endif
 
 #endif  // HARDWARE_MAPPING_H
